52ea22efd687b0f765d533cc746cce5c14b17ede
Deanna Henkle <49732744+henkled@users.noreply.github.com>
2020-02-03 13:37:42 -0800

    Create ButtonManager.cs
    Uploading a code file with some typos to be edited.

---
+++ ButtonManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityChooChooEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoMisBehaviour
+{
+    public void ExitGameButton()
+    {
+        Application.Quite();
+    }
+
+}
+
+
+// There are three typos in the code that can be fixed.
